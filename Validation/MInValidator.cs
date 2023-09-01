36152a76d2d8a19047523b62c06364763acdedd2
Philip Sutton <Philip.Sutton@ensek.co.uk>
2023-09-01 12:09:26 +0100

    Add asyn for miv validator

--- Validation/MInValidator.cs
+++ Validation/MInValidator.cs
@@ -10,5 +10,14 @@
             if (personScore.Score < 0) return "Score is too low for person {personScore.Id}";
             return null;
         }
+
+        public Task<string?> IsValidAsync(PersonScore personScore)
+        {
+            ArgumentNullException.ThrowIfNull(personScore);
+            ArgumentNullException.ThrowIfNull(personScore.Id);
+            ArgumentNullException.ThrowIfNull(personScore.Score);
+            if (personScore.Score < 0) return Task.FromResult((string?)"Score is too low for person {personScore.Id}");
+            return Task.FromResult((string?)null);
+        }
     }
 }
